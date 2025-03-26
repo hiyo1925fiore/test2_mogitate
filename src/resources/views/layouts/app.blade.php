8a350ce5874d3ed870ad37284adfcc02cce146ef
hinako shimamori <isola16858foresta198401@gmail.com>
2025-03-26 23:58:21 +0900

    add: 共通レイアウト

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mogitate</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <h1 class="header__heading">mogitate</h1>
+        </div>
+    </header>
+
+    <div class="content">
+        @yield('content')
+    </div>
+</body>
+
+</html>
